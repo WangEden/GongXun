cc1c23a99d9735836bdaa7c95c7af40429e50334
WangEden <wangeden_@outlook.com>
2023-09-22 22:21:18 +0800

    take 3 img

--- script/takeshot.py
+++ script/takeshot.py
@@ -8,7 +8,14 @@ cap.set(3, 640)
 cap.set(4, 480)
 cap.set(6, cv2.VideoWriter.fourcc(*'MJPG'))
 
-n = 1
+n = 0
+with open( "img.txt", "r") as file:
+    s = file.read()
+    n = int(s)
+    print(s)
+
+t = n
+
 while n > 0:
     ret, frame = cap.read()
     if ret:
@@ -16,4 +23,7 @@ while n > 0:
     if cv2.waitKey(10) & 0xFF == ord("q"):
         cv2.imwrite(f"./{n}.jpg", frame)
         print(f"take {n} photo")
-        n -= 1
+        n = 0
+
+with open( "img.txt", "w") as file:
+    file.write(str(t+1))
