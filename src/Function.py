9d5a1df9cd7fc5c7e4229470a834103de1e4429f
Gyudu33 <102165364+Gyudu33@users.noreply.github.com>
2023-10-10 02:02:43 +0800

    complete task3

--- src/Function.py
+++ src/Function.py
@@ -108,7 +108,8 @@ def parseItemCatchQueue(qr_result, q1, q2):
 
 #定义数据包，格式为2个帧头+4个字符数据+2个半整型数据+帧尾（11byte）
 #4个字符传输命令名，2个int传输xy方向的偏差
-def send_data(uart, a, b, c, d, i, f):
+def send_data(uart, cmd, i, f):
+    a, b, c, d = cmd
     data = struct.pack("<bbbbbbhhb", # 四个字符作为命令, 两个浮点作为xy偏差
                         0x2C,      # 帧头1      ','
                         0x3C,      # 帧头2      '<'
@@ -120,6 +121,66 @@ def send_data(uart, a, b, c, d, i, f):
                         int(f), # 半整型数据2
                         0x3E)      # 帧尾       '>'
     uart.write(data)
+
+
+# 图像预处理
+def precondition(_img):
+    _ = cv2.pyrMeanShiftFiltering(_img, 15, 20)
+    _ = cv2.GaussianBlur(_, (3, 3), 0)
+    return _
+
+
+# 获取命令
+def getMessage(node, tag):
+    _res = node.find(tag).text
+    return _res
+
+
+# bbox: [box1, box2, ...]
+# box: [左上角点x, 左上角点y, 宽度, 高度, ...]
+def mask_find_b_boxs(_mask):
+    retval, labels, stats, centroids = cv2.connectedComponentsWithStats(_mask, connectivity=8)  # connectivity参数的默认值为8
+    stats = stats[stats[:, 4].argsort()]
+    return stats[:-1]
+
+
+# 获取阈值
+def getColorThreshold(root, tag, rank):
+    _min, _max = [], []
+    colorNode = root.find(f'color[@category="{tag}"]')
+    floors = colorNode.findall('./*/floor')
+    ceilings = colorNode.findall('./*/ceiling')
+    for i in range(3):
+        _min.append(int(floors[i].text))
+        _max.append(int(ceilings[i].text))
+    _min = np.array(_min)
+    _max = np.array(_max)
+    rank[0] = _min
+    rank[1] = _max
+
+
+def get_the_most_credible_box(b_box):
+    global XCenter, YCenter
+    if len(b_box) == 0:
+        return None
+    if len(b_box) == 1:
+        return b_box[0]
+    b_box = sorted(b_box, key=lambda box: abs(box[0] + box[2] / 2 - XCenter))
+    # print("by dx:\n", b_box)
+    b_box = sorted(b_box, key=lambda box: abs(box[1] + box[3] / 2 - YCenter))
+    # print("by dy:\n", b_box)
+    b_box = sorted(b_box, key=lambda box: box[4], reverse=True)
+    # print("by area:\n", b_box)
+    return b_box[0]
+
+
+def uDistanceToDx(ud, h):
+    if h == 16:
+        return int(ud * 25 / 120 * 10)
+    elif h == 30:
+        return 0
+    else:
+        return 0
 
 
 if __name__ == "__main__":
--- src/main.py
+++ src/main.py
@@ -1,67 +1,143 @@
-import Function as F
+from Function import *
 from xml.etree import ElementTree as ET
 import serial
 import cv2
-# import numpy as np
+import numpy as np
 
 
 # 声明串口
-uart = serial.Serial(port="/dev/ttyAMA0", 
-                     baudrate=115200, 
-                     bytesize=8, 
-                     parity=serial.PARITY_NONE, 
-                     stopbits=1)
+uart = serial.Serial(
+    port="/dev/ttyAMA0",
+    baudrate=115200,
+    bytesize=8,
+    parity=serial.PARITY_NONE,
+    stopbits=1,
+)
 # 参数文件
 paraDomTree = ET.parse("./parameter.xml")
-message = paraDomTree.find('message')
+messageNode = paraDomTree.find("message")
+ringThresholdNode = paraDomTree.find('threshold[@tag="ring"]')
+itemThresholdNode = paraDomTree.find('threshold[@tag="item"]')
 # 相机目录
 cameraTopPath = "/dev/cameraTop"
 cameraIncPath = "/dev/cameraInc"
-cameraInc = F.VideoCapture(cameraIncPath)
-cameraTop = F.VideoCapture(cameraTopPath)
+# cameraInc = F.VideoCapture(cameraIncPath)
+# cameraTop = F.VideoCapture(cameraTopPath)
 # 存储物块领取顺序
-catchQueue1 = [] # 第一趟
-catchQueue2 = [] # 第二趟
+catchQueue1 = []  # 第一趟 ['r', 'g', 'b']
+catchQueue2 = []  # 第二趟 ['g', 'b', 'r']
+# 存储阈值
+redThreshold = [None, None]
+greenThreshold = [None, None]
+blueThreshold = [None, None]
+
+# def start():
+#     cv2.namedWindow("main")
+#     cv2.resizeWindow("main", 1024, 480)
+#     screen = np.ones(480, 1024) * 255
+#     cv2.rectangle(screen)
 
 
-def task1(): # 任务一、读取二维码
-    global uart, cameraInc, catchQueue1, catchQueue2
+def task1():  # 任务一、读取二维码
+    global uart, catchQueue1, catchQueue2, cameraTopPath, messageNode
+
     if not uart.isOpen():
         print("Serial Not Open!")
+
     # 等待车已停靠在二维码旁的信号
     responce = None
-    # print("wish: ", message.find('arrived').text)
     while True:
-        responce = uart.read(2).decode('utf-8')
+        responce = uart.read(4).decode("utf-8")
         if responce is not None:
-            # print("recv: ", responce)
-            if responce == message.find('arrived').text:
-                print("succeed:", responce)
+            print("recv: ", responce)
+            if responce == getMessage(messageNode, 'arrived'):
+                print("succeed recv:", responce)
+                print("start qrcode recognize ...")
                 # 接收到到达识别二维码区域的信号
                 break
-        # print("lloop")
-    
+
+    camera = VideoCapture(cameraTopPath)
     while True:
-        img = cameraInc.read()
-        # print(img)
-        # cv2.imshow("cameraInc", img)
-        cv2.waitKey(24)
-        qrcode_result = F.getQRCodeResult(img)
-        if qrcode_result is not None:
-            print(qrcode_result)
-            F.parseItemCatchQueuese(qrcode_result, catchQueue1, catchQueue2)
-            print("q1: ", catchQueue1, "q2: ", catchQueue2)
-            uart.write("QROK".encode("gbk"))
+        img = camera.read()
+        if img is not None:
+            cv2.imshow("qrcode", img)
+            cv2.waitKey(1)
+            qrcode_result = getQRCodeResult(img)
+            if qrcode_result is not None:
+                print("qrcode result: ", qrcode_result)
+                parseItemCatchQueue(qrcode_result, catchQueue1, catchQueue2)
+                print("q1: ", catchQueue1, "q2: ", catchQueue2)
+                cmd = getMessage(messageNode, 'qrComplete').split()
+                send_data(uart, cmd, 0, 0)
 
 
-def task2(): # 前往原料区、识别圆盘、校准物块、取物块
+def task2():  # 前往原料区、识别圆盘、校准物块、取物块
     pass
 
 
-def task3(): # 前往粗加工区、识别色环颜色、校准位置、按顺序放置物块
-    pass
+def task3():  # 前往粗加工区、识别色环颜色、校准位置、按顺序放置物块
+    global uart, messageNode, cameraIncPath
+    camera = VideoCapture(cameraIncPath)
+    while True:
+        response = uart.read(4).decode("utf-8")
+        print("waiting for start wei tiao command ...")
+        print("recv: ", response)
+        if response == getMessage(messageNode, 'arrived'):
+            print("start wei tiao ...")
+            break
+    
+    flag = True
+    while flag:
+        frame = camera.read()
+        if frame is None:
+            print("no img !")
+            continue
+        # cv2.imshow("wei tiao", frame)
+        # cv2.waitKey(1)
+        print("processing img ...")
+        img_bgr = precondition(frame)
+        img_hsv = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2HSV)
+        mask = cv2.inRange(img_hsv, blueThreshold[0], blueThreshold[1])
+        mask = cv2.medianBlur(mask, 3)
+        bbox = mask_find_b_boxs(mask)
+        img_note = frame.copy()
+        most_credible_box = get_the_most_credible_box(bbox)
+        if most_credible_box is not None:
+            p1 = tuple([most_credible_box[0], most_credible_box[1]])
+            p2 = tuple([most_credible_box[0] + most_credible_box[2], most_credible_box[1] + most_credible_box[3]])
+            cx = int((p1[0] + p2[0]) / 2)
+            cy = int((p1[1] + p2[1]) / 2)
+            cv2.rectangle(img_note, p1, p2, (255, 0, 0), 1)
+            cv2.putText(img_note, f"({cx}, {cy})", p1, cv2.FONT_HERSHEY_SIMPLEX, 0.75, (255, 255, 255), 1)
+            cv2.circle(img_note, (cx, cy), 4, (64, 128, 255), -1)
+            udx = cx - XCenter
+            udy = cy - YCenter
+            cv2.putText(img_note, f"({udx}, {udy})", (cx, cy), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (255, 255, 255), 1)
+            cv2.line(img_note, (320, 240), (cx, cy), (255, 0, 0), 2)
+        cv2.imshow("img_note", img_note)
+        cv2.waitKey(1)
+        cmd = getMessage(messageNode, 'tweak').split()
+        dx = uDistanceToDx(udx, 16)
+        dy = uDistanceToDx(udy, 16)
+        if abs(udx) < 3 and abs(udy) < 3:
+            cmd = getMessage(messageNode, 'calibrOk').split()
+            dx, dy = 0, 0
+            flag = False
+        send_data(uart, cmd, dx, dy)
+        while flag:
+            response = uart.read(4).decode("utf-8")
+            print("waiting for weitiao complete...")
+            print("recv: ", response)
+            if response == getMessage(messageNode, 'wtok'):
+                print("start wei tiao ...")
+                break
+    print("task3 ok")
+
+# def show_img(img):
+#     cv2.imshow("hou tai img", img)
+#     cv2.waitKey(24)
 
 
 if __name__ == "__main__":
-    cv2.namedWindow("cameraInc", cv2.WINDOW_NORMAL)
-    task1()
+    getColorThreshold(itemThresholdNode, 'blue', blueThreshold)
+    task3()
