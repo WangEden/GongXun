cdfd3c2c7e33019c55d448d7c5d2eb442bacb7c2
WangEden <wangeden_@outlook.com>
2023-10-13 23:55:02 +0800

    syn

--- script/detect_circle.py
+++ script/detect_circle.py
@@ -19,8 +19,10 @@ def calc_the_most_frequent_position_of_points(points):
 try:
     while True:
         # ret, frame = cap.read() 
-        frame = cv2.imread("匹配时mask3.jpg")
-        # frame = cv2.cvtColor(frame, cv2.COLOR_HSV2BGR)
+        # frame = cv2.imread("匹配时mask3.jpg")
+        frame = cv2.imread("匹配时hsv3.jpg")
+        frame = cv2.cvtColor(frame, cv2.COLOR_HSV2BGR)
+        cv2.imwrite("匹配时bgr.jpg", frame)
         # if not ret:
         #     print("no img")
         #     continue
@@ -38,6 +40,8 @@ try:
         erode_kernel = np.ones((1, 1), dtype=np.uint8)
         erosion_binary = cv2.erode(binary, kernel=erode_kernel, iterations=1)
 
+        erosion_binary = cv2.medianBlur(erosion_binary, 3)
+
         # 拓展图片维度
         erosion_binary_channel3 = np.repeat(erosion_binary[:, :, np.newaxis], repeats=3, axis=2)
 
@@ -46,17 +50,18 @@ try:
 
         text = None
         # 在二值化画面中查找圆形
-        circles = cv2.HoughCircles(erosion_binary, cv2.HOUGH_GRADIENT, 1, 100)
+        circles = cv2.HoughCircles(erosion_binary, 
+                                   cv2.HOUGH_GRADIENT, 
+                                   100, 170, minRadius=50, maxRadius=150)
         if circles is not None:
             print("有圆形")
             circles = np.round(circles[0, :]).astype('int')
             for (x, y, r) in circles:
-                text = "position("+str(x)+", "+str(y)+")"
+                text = "radius:"+str(r)
                 cv2.circle(img_note, (x, y), r, (0, 255, 0), 4)
                 cv2.rectangle(img_note, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
                 # break
-
-        cv2.putText(img_note, text, (5,50), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 255), 2)
+                cv2.putText(img_note, text, (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 255), 2)
         output = np.hstack([erosion_binary_channel3, img_note])
         output = cv2.resize(output, dsize=None,fx=0.5, fy=0.5, interpolation=cv2.INTER_LINEAR)
 
