78e421b1eac7594c2dce05bc17f3d83e26cddfee
Gyudu33 <102165364+Gyudu33@users.noreply.github.com>
2023-10-19 03:36:30 +0800

    Update test_screen.py

--- test_screen/test_screen.py
+++ test_screen/test_screen.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 from PIL import Image, ImageDraw, ImageFont
+import threading
 
 
 def cv2AddChineseText(img, text, position, textColor=(0, 255, 0), textSize=30):
@@ -16,6 +17,17 @@ def cv2AddChineseText(img, text, position, textColor=(0, 255, 0), textSize=30):
     # 转换回OpenCV格式
     return cv2.cvtColor(np.asarray(img), cv2.COLOR_RGB2BGR)
 
+
+class myThread(threading.Thread):
+    def __init__(self, name=None):
+        super(myThread, self).__init__(name=name)
+
+    def run(self):
+        print('=> Thread %s is running ...' % self.name)
+
+thread = myThread()
+thread.start()
+thread.join()
 
 
 cv2.namedWindow("123", cv2.WINDOW_NORMAL)
