c9fb5550b8baefcd5edbc1e11d13cce8aa7e167e
WangEden <wangeden_@outlook.com>
2023-10-18 22:52:17 +0800

    syn

--- script/test.py
+++ script/test.py
@@ -1,53 +1,29 @@
-import cv2
-import numpy as np
+import serial, struct
 
-cameraTop = "/dev/cameraTop"
-cameraInc = "/dev/cameraInc"
+uart = serial.Serial( # 声明串口
+    port="/dev/ttyAMA0",
+    baudrate=115200,
+    bytesize=8,
+    parity=serial.PARITY_NONE,
+    stopbits=1,
+    timeout=0,
+    dsrdtr=True,
+)
 
-w = 480
-h = 640
-
-cap = cv2.VideoCapture(cameraTop)
-cap.set(3, w)
-cap.set(4, h)
-cap.set(6, cv2.VideoWriter.fourcc(*'MJPG'))
-
-# print("宽度：", cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-# print("高度：", cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-# print("编码：", cap.get(cv2.CAP_PROP_FOURCC))
-
-filename = "./output.mp4"
-
-fourcc = cv2.VideoWriter_fourcc(*'MJPG')
-out = cv2.VideoWriter(filename, fourcc, 30.0, (h, w))
-
-srcPoint = np.uint8([[0, h], [0, 0], [w, 0]])
-desPoint = np.uint8([[0, 0], [h, 0], [h, w]])
-
-M = cv2.getAffineTransform(srcPoint, desPoint)
-pad = np.zeros((2 * h, h, 3), dtype=np.uint8)
-
-ret, frame = cap.read()
-
-# print(pad.shape)
-mask1, mask2 = None
-
-# if ret:
-#     mask1 = np.zeros(frame.shape[0:2], dtype=np.uint8)
-#     mask2 = np.zeros(frame.shape[0:2], dtype=np.uint8)
+def send_data(cmd:list, i, f):
+    a, b, c, d = cmd
+    data = struct.pack("<bbbbbbhhb", # 四个字符作为命令, 两个浮点作为xy偏差
+                        0x2C,      # 帧头1      ','
+                        0x3C,      # 帧头2      '<'
+                        ord(str(a)), # 字符1
+                        ord(str(b)), # 字符2
+                        ord(str(c)), # 字符3
+                        ord(str(d)), # 字符4
+                        int(i), # 半整型数据1
+                        int(f), # 半整型数据2
+                        0x3E)      # 帧尾       '>'
+    uart.write(data)
 
 while True:
-    ret, frame = cap.read()
-    if not ret:
-        break
-    srcImg = cv2.resize(frame, (2 * h, h))
-    desImg = cv2.warpAffine(frame, M, (h, w))
-    desImg = cv2.resize(desImg, (2 * h, h))
-
-    cv2.imshow("srcImg", srcImg)
-    cv2.imshow("desImg", desImg)
-
-    cv2.waitKey(0)
-    # mask1 = cv2.
-
-    
+    print("recv:", uart.read(4).decode('utf-8'))
+    # send_data(['a','b','c','d'], 0, 0)
