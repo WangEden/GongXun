c276db0cca2a8d9bab8e721e272eac78f04ae845
WangEden <wangeden_@outlook.com>
2023-10-19 17:31:58 +0800

    syn

--- test_screen/test_screen.py
+++ test_screen/test_screen.py
@@ -1,7 +1,6 @@
 import cv2
 import numpy as np
 from PIL import Image, ImageDraw, ImageFont
-import threading
 
 
 def cv2AddChineseText(img, text, position, textColor=(0, 255, 0), textSize=30):
@@ -18,29 +17,20 @@ def cv2AddChineseText(img, text, position, textColor=(0, 255, 0), textSize=30):
     return cv2.cvtColor(np.asarray(img), cv2.COLOR_RGB2BGR)
 
 
-class myThread(threading.Thread):
-    def __init__(self, name=None):
-        super(myThread, self).__init__(name=name)
-
-    def run(self):
-        print('=> Thread %s is running ...' % self.name)
-
-thread = myThread()
-thread.start()
-thread.join()
+def handler(event, x, y, flags, param):
+    global flag
+    flag = False
 
 
 cv2.namedWindow("123", cv2.WINDOW_NORMAL)
 cv2.setWindowProperty("123", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
 screen = np.ones((600, 1024), dtype=np.uint8) * 255
-
 string = "郭昕昌"
 XCenter, YCenter = 320, 240
 screen=cv2AddChineseText(screen, string, (XCenter, YCenter), (0, 0, 0), 60)
-cv2.imshow("123", screen)
-cv2.waitKey(0)
-
-while True:
-    print("asc")
-
-
+cv2.setMouseCallback("123", handler)
+print("子程序运行中...")
+flag = True
+while flag:
+    cv2.imshow("123", screen)
+    cv2.waitKey(25)
