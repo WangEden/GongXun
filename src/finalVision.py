c075a213da55a75f6c4dbb1a4bb4c8c2dcb72e95
WangEden <68456937+WangEden@users.noreply.github.com>
2023-10-25 13:14:33 +0800

    ,

--- src/finalVision.py
+++ src/finalVision.py
@@ -18,12 +18,12 @@ def fineTuneItemF(_threshold: list, category: str, loop: int):
     # 测距离比例, 不通信 # # # # # # # # # # # # # # 
     # 设置相机参数
     cap = cv2.VideoCapture("/dev/cameraInc")
-    capSet = cap.set(3, 640)
-    capSet = cap.set(4, 480)
-    capSet = cap.set(cv2.CAP_PROP_AUTO_WB, 1)
-    capSet = cap.set(cv2.CAP_PROP_AUTO_EXPOSURE, 1)
+    cap.set(3, 640)
+    cap.set(4, 480)
+    cap.set(cv2.CAP_PROP_AUTO_WB, 1)
+    cap.set(cv2.CAP_PROP_AUTO_EXPOSURE, 1)
     capSet = cap.set(6, cv2.VideoWriter.fourcc(*"MJPG"))
-    if (capSet):
+    if capSet:
         print("相机参数设置成功")
 
     # 判断圆盘是否在转动 # # # # # # # # # # # #
@@ -33,7 +33,7 @@ def fineTuneItemF(_threshold: list, category: str, loop: int):
     is_plate_move = True
     while is_plate_move:
         end_time = time.time()
-        if (end_time - start_time > 0.1):
+        if end_time - start_time > 0.1:
             start_time = time.time()
             ret, last_frame = cap.read()
         
@@ -43,13 +43,24 @@ def fineTuneItemF(_threshold: list, category: str, loop: int):
     # # # # # # # # # # # # # # # # # # # # # #
 
     # 计算距离比例 # # # # # # # # # # # # # # #
+    #  物料像素长宽要大于100
     XCenter, YCenter = 320, 240
     ret, frame = cap.read()
     
+    # 匹配颜色
     img = cv2.GaussianBlur(frame, (3, 3), 0)
     img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     img_hsv = cv2.erode(img_hsv, None, iterations=2)
 
+    mask1 = cv2.inRange(img_hsv, threshold[0][0], threshold[0][1])
+    mask1 = cv2.medianBlur(mask1, 3)
+    mask2 = cv2.inRange(img_hsv, threshold[1][0], threshold[1][1])
+    mask2 = cv2.medianBlur(mask2, 3)
+    mask3 = cv2.inRange(img_hsv, threshold[2][0], threshold[2][1])
+    mask3 = cv2.medianBlur(mask3, 3)
+    mask = mask1 + mask2 + mask3
+
+    # 查找物块
     
     # # # # # # # # # # # # # # # # # # # # # #
 
--- src/look.py
+++ src/look.py
@@ -3,41 +3,57 @@ import time
 import numpy as np
 
 
-video_file = "./videos/video.mp4"
-cap = cv2.VideoCapture(0)  # 读取视频文件，参数设置为0表示从摄像头获取图像
+img_path = "take1.jpg"
+img = cv2.imread(img_path)
 
 
-def moving_detect(frame1, frame2):
-    img1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
-    img2 = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
-    grey_diff = cv2.absdiff(img1, img2)  # 计算两幅图的像素差
-    change = np.average(grey_diff)
-
-    if change > 10:  # 当两幅图的差异大于给定的值后，认为画面有物体在动
-        cv2.putText(frame2, 'moving', (100, 30), 2, 1, (0, 255, 0), 2, cv2.LINE_AA)
-    else:
-        cv2.putText(frame2, 'quiet', (100, 30), 2, 1, (255, 0, 0), 2, cv2.LINE_AA)
-    cv2.imshow("output", frame2)
+def mouseHandler(e, x, y, f, p):
+    if e == cv2.EVENT_LBUTTONDOWN:
+        print("click: ", x, y)
 
 
-_, frame1 = cap.read()
-img1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)  # 将图片转为灰度图，第一个返回值表示是否转换成功，第二个返回值就是灰度图了
-start = time.time()
-while True:
-    end = time.time()
-    if (end - start > 0.1):  # 每隔2秒拍一幅图，比较前后两幅图的差异
-        start = time.time()
-        _, frame1 = cap.read()
-
-    _, frame2 = cap.read()
-    moving_detect(frame1, frame2)
-    if cv2.waitKey(5) & 0xFF == ord('q'):  # 按下q停止运行程序
-        break
+cv2.namedWindow("img")
+cv2.setMouseCallback("img", mouseHandler)
 
 
-# 最后，关闭所有窗口
-cap.release()
-cv2.destroyAllWindows()
+cv2.imshow("img", img)
+cv2.waitKey(0)
+
+
+# cap = cv2.VideoCapture(0)  # 读取视频文件，参数设置为0表示从摄像头获取图像
+#
+#
+# def moving_detect(frame1, frame2):
+#     img1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
+#     img2 = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
+#     grey_diff = cv2.absdiff(img1, img2)  # 计算两幅图的像素差
+#     change = np.average(grey_diff)
+#
+#     if change > 10:  # 当两幅图的差异大于给定的值后，认为画面有物体在动
+#         cv2.putText(frame2, 'moving', (100, 30), 2, 1, (0, 255, 0), 2, cv2.LINE_AA)
+#     else:
+#         cv2.putText(frame2, 'quiet', (100, 30), 2, 1, (255, 0, 0), 2, cv2.LINE_AA)
+#     cv2.imshow("output", frame2)
+#
+#
+# _, frame1 = cap.read()
+# img1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)  # 将图片转为灰度图，第一个返回值表示是否转换成功，第二个返回值就是灰度图了
+# start = time.time()
+# while True:
+#     end = time.time()
+#     if (end - start > 0.1):  # 每隔2秒拍一幅图，比较前后两幅图的差异
+#         start = time.time()
+#         _, frame1 = cap.read()
+#
+#     _, frame2 = cap.read()
+#     moving_detect(frame1, frame2)
+#     if cv2.waitKey(5) & 0xFF == ord('q'):  # 按下q停止运行程序
+#         break
+#
+#
+# # 最后，关闭所有窗口
+# cap.release()
+# cv2.destroyAllWindows()
 
 # import cv2
 # import numpy as np
