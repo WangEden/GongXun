8f217020e06abe192abb0baaf55ad1f868450253
WangEden <68456937+WangEden@users.noreply.github.com>
2023-11-07 20:47:44 +0800

    Delete build directory

--- build/CMakeFiles/GongXun.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for GongXun.
-# This may be replaced when dependencies are built.
--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: F:/ARM/GongXun
-# Build directory: F:/ARM/GongXun/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
